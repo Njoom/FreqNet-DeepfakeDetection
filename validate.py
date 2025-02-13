12e6f92723a7169449ff6f657808991fef74f043
Njoom <85446073+Njoom@users.noreply.github.com>
2025-02-13 11:50:55 +0300

    Update validate.py

--- validate.py
+++ validate.py
@@ -17,6 +17,11 @@ def validate(model, opt):
             y_true.extend(label.flatten().tolist())
 
     y_true, y_pred = np.array(y_true), np.array(y_pred)
+    
+    # Reshape y_pred if it's 1D
+    if len(y_pred.shape) == 1:
+        y_pred = y_pred.reshape(-1, 1)  # Reshape to (n_samples, 1)
+        
     r_acc = accuracy_score(y_true[y_true==0], y_pred[y_true==0] > 0.5)
     f_acc = accuracy_score(y_true[y_true==1], y_pred[y_true==1] > 0.5)
     acc = accuracy_score(y_true, y_pred > 0.5)
