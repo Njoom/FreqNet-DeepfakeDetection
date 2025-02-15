4fe635523790768126fcc2141ec6780f985daf87
Njoom <85446073+Njoom@users.noreply.github.com>
2025-02-15 17:59:26 +0300

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -5,7 +5,7 @@ class TestOptions(BaseOptions):
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
         # parser.add_argument('--dataroot')
-        parser.add_argument('--model_path', default = './4-classes-freqnet.pth',type=str, help='Path to the trained model file (e.g., model_epoch_last.pth)')
+        parser.add_argument('--model_path', default = './checkpoints/experiment_name2025_02_15_14_53_59/model_epoch_last.pth',type=str, help='Path to the trained model file (e.g., model_epoch_last.pth)')
         parser.add_argument('--no_resize', action='store_true')
         parser.add_argument('--no_crop', action='store_true')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
