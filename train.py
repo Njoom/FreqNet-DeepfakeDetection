acd2c3af61b868867a1338c2b613e61703012c63
Njoom <85446073+Njoom@users.noreply.github.com>
2025-02-15 17:37:09 +0300

    Update train.py

--- train.py
+++ train.py
@@ -114,6 +114,8 @@ if __name__ == '__main__':
 
     model.eval();testmodel()
     save_dir = './checkpoints/'  # Specify your desired directory
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
     model.save_networks(os.path.join(save_dir, 'model_epoch_last.pth'))  # Save the model with a specific name
     
     
